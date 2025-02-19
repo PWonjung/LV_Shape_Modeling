d943160973cf1d14421927987f260ee3320bdb9f
wonjungpark <fabiola@kaist.ac.kr>
2025-02-19 12:29:51 +0000

    add py

---
+++ ipynb/MICCAI-LV/run_middle_step3.py
@@ -0,0 +1,58 @@
+import multiprocessing
+import os
+import argparse
+import glob
+import shutil
+import time
+
+label = 'mid_TRS3'
+def process_input(data_path, sub_id):
+    command = f"python ../../voxel2mesh/hippo_lv_trs_train.py --data_path {data_path} --tag {label}  --learning_rate 0.0005 --sub_id {sub_id} --gpu 1 --lda 2 0 0 0 0 0 0"
+    # pm, cf, edge, lap, norm_con, l2_vert, l2_norm
+    
+    print(command)
+    import subprocess
+    subprocess.run(command, shell=True)
+def main():
+
+    filelist = glob.glob(f"/root/LV/LV/2502LV/pcd_scaled/L/notail/*.pkl")
+    filelist = filelist[len(filelist)//2:]
+    print(filelist)
+    # print(filelist)python
+    num_item = 5
+    filelist = filelist
+    # print(len(filelist))
+    # print(filelist)
+    # return
+    for i in range(len(filelist)//num_item+1):
+        processes = []                                                                                                                                       
+
+        if (i+1)*num_item > len(filelist):
+            folders = filelist[i*num_item:]
+        else:
+            folders = filelist[i*num_item:(i+1)*num_item]
+            print(len(folders))
+            print(folders)
+
+        for fold in folders:
+            sub_id = fold.split("/")[-1].split("_")[1]
+            data_path = rf"{fold}"
+            exist5000 = glob.glob(f"/root/LV/lv-parametric-modelling/ipynb/MICCAI-LV/results/{label}/out/{sub_id}/5000*.npy")
+
+            if exist5000==[]:
+            # print(f"{sub_id=}")
+                p = multiprocessing.Process(target=process_input, args=(data_path, sub_id,))
+                processes.append(p)
+                p.start()
+            else:
+                print(f"{sub_id} is already exists.")
+    
+        for p in processes:
+            p.join()
+
+
+if __name__ == "__main__":
+    main()
+
+# python C:\Users\v1wpark\Documents\Projects\lv-parametric-modelling\voxel2mesh\template_redistribute.py --data_path C:\Users\v1wpark\Documents\Projects\lv-parametric-modelling\ipynb\edinburgh\template_mesh_r.pkl --tag template_r  --learning_rate 0.0005 --sub_id template_r
+# python ../../voxel2mesh/hippo_p2p.py --data_path /root/LV/LV/0124_hippo/synthseg_l/LBC360002_synthseg_l_opt_data.pkl --tag synthseg_l_new_pm  --learning_rate 0.0005 --sub_id LBC360002 --gpu 1 --lda 0 0.5 1500 5 1 1 1
